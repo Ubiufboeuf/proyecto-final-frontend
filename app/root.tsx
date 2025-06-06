3571aeb9c39ff3cd7aa94d0dde7f61db9834840e
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-06-06 14:43:44 -0300

    chore: cambiar idioma del documento de ingles a español

--- app/root.tsx
+++ app/root.tsx
@@ -15,7 +15,7 @@ import { useEffect } from 'react';
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
