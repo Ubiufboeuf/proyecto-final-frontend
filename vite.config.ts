019ba4a2890ee714e36801ef7bda5a1eeac555cb
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-06-15 17:12:22 -0300

    chore: :wrench: quitar hosts válidos para pruebas

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
-    allowedHosts: ['6957-190-64-207-178.ngrok-free.app']
+    allowedHosts: ['']
   }
 })
