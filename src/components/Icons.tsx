e2d4244c1ba0f5908aedc66246ad41e2e581c581
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-06-19 13:42:24 -0300

    feat: añadir iconos svg

---
+++ src/components/Icons.tsx
@@ -0,0 +1,232 @@
+export const IconBus = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M8 6v6' />
+    <path d='M15 6v6' />
+    <path d='M2 12h19.6' />
+    <path d='M18 18h3s.5-1.7.8-2.8c.1-.4.2-.8.2-1.2 0-.4-.1-.8-.2-1.2l-1.4-5C20.1 6.8 19.1 6 18 6H4a2 2 0 0 0-2 2v10h3' />
+    <circle cx='7' cy='18' r='2' />
+    <path d='M9 18h5' />
+    <circle cx='16' cy='18' r='2' />
+  </svg>
+)
+
+export const IconMenu = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M4 8l16 0' />
+    <path d='M4 16l16 0' />
+  </svg>
+)
+
+export const IconMail = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <rect width='20' height='16' x='2' y='4' rx='2' />
+    <path d='m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7' />
+  </svg>
+)
+export const IconPhone = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z' />
+  </svg>
+)
+export const IconLocation = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z' />
+    <circle cx='12' cy='10' r='3' />
+  </svg>
+)
+export const IconPointerArrow = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <polygon points='3 11 22 2 13 21 11 13 3 11' />
+  </svg>
+)
+export const IconCoffee = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M10 2v2' />
+    <path d='M14 2v2' />
+    <path d='M16 8a1 1 0 0 1 1 1v8a4 4 0 0 1-4 4H7a4 4 0 0 1-4-4V9a1 1 0 0 1 1-1h14a4 4 0 1 1 0 8h-1' />
+    <path d='M6 2v2' />
+  </svg>
+)
+export const IconWifi = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M12 20h.01' />
+    <path d='M2 8.82a15 15 0 0 1 20 0' />
+    <path d='M5 12.859a10 10 0 0 1 14 0' />
+    <path d='M8.5 16.429a5 5 0 0 1 7 0' />
+  </svg>
+)
+export const IconAir = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M6 12H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2' />
+    <path d='M6 8h12' />
+    <path d='M18.3 17.7a2.5 2.5 0 0 1-3.16 3.83 2.53 2.53 0 0 1-1.14-2V12' />
+    <path d='M6.6 15.6A2 2 0 1 0 10 17v-5' />
+  </svg>
+)
+export const IconStar = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <polygon points='12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2' />
+  </svg>
+)
+export const IconShield = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M20 13c0 5-3.5 7.5-7.66 8.95a1 1 0 0 1-.67-.01C7.5 20.5 4 18 4 13V6a1 1 0 0 1 1-1c2 0 4.5-1.2 6.24-2.72a1.17 1.17 0 0 1 1.52 0C14.51 3.81 17 5 19 5a1 1 0 0 1 1 1z' />
+  </svg>
+)
+export const IconPeople = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2' />
+    <circle cx='9' cy='7' r='4' />
+    <path d='M22 21v-2a4 4 0 0 0-3-3.87' />
+    <path d='M16 3.13a4 4 0 0 1 0 7.75' />
+  </svg>
+)
+export const IconClock = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <circle cx='12' cy='12' r='10' />
+    <polyline points='12 6 12 12 16 14' />
+  </svg>
+)
