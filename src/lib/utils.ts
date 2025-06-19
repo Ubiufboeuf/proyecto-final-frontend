bca8e39b8a0d6bee314735c3fd1463cbb7c7da90
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-06-19 13:51:09 -0300

    chore: crear utils.ts con funciones y variables útiles en general

---
+++ src/lib/utils.ts
@@ -0,0 +1,21 @@
+import { Temporal } from 'temporal-polyfill'
+
+const padStart = (val: number, length: number, fill: string) => val.toString().padStart(length, fill)
+
+export function parseTimestamp (timestamp: number) {
+  const zdt = Temporal.Instant.fromEpochMilliseconds(timestamp).toZonedDateTimeISO('America/Montevideo')
+  return `
+    ${padStart(zdt.hour, 2, '0')}:${padStart(zdt.minute, 2, '0')}:${padStart(zdt.second, 2, '0')}
+    -
+    ${padStart(zdt.day, 2, '0')}/${padStart(zdt.month, 2, '0')}/${padStart(zdt.year, 2, '0')}`
+}
+
+export const title = (title: string) => title ? `${title} - Berruti Turismo` : 'Berruti Turismo'
+
+export const links = [
+  { name: 'Inicio', link: '/' },
+  { name: 'Servicios', link: '/services' },
+  { name: 'Rutas', link: '/routes' },
+  { name: 'Nosotros', link: '/about' },
+  { name: 'Contacto', link: '/contact' }
+]
