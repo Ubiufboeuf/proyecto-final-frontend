ada91b7caf89510191654334bb48c0223b2950a2
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-06-19 18:32:15 -0300

    fix: :lipstick: arreglar estilos para funcionar en producción y desarrollo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config'
-
 import preact from '@astrojs/preact'
-
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 
