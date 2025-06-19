bfb0652ab3fb232071cd835778956664fdc4da29
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-06-19 13:33:49 -0300

    feat: crear componente Icon para crear iconos más rápidamente
    ya tiene los estilos base para la mayoría de iconos establecidos

---
+++ src/components/Icon.tsx
@@ -0,0 +1,12 @@
+import type { CSSProperties, ReactNode } from 'preact/compat'
+
+export function Icon ({ children, class: className, style }: { children: ReactNode, class?: string, style?: CSSProperties }) {
+  return (
+    <div 
+      className={`max-h-full max-w-full aspect-square [&>*]:h-full [&>*]:w-full [&>*]:aspect-square [&>*]:max-h-full [&>*]:max-w-full [&>*]:select-none ${className}`}
+      style={style}
+    >
+      {children}
+    </div>
+  )
+}
