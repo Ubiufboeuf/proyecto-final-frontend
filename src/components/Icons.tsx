aa47a1afb12e5cf19dcebead5974931ec41f87fd
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-06-28 03:35:13 -0300

    feat: añadir iconos a la lista

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -231,3 +231,39 @@ export const IconClock = () => (
     <polyline points='12 6 12 12 16 14' />
   </svg>
 )
+
+export const IconX = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M18 6l-12 12' />
+    <path d='M6 6l12 12' />
+  </svg>
+)
+
+export const IconCopy = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z' />
+    <path d='M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1' />
+  </svg>
+)
