7dfbbe171caa41fccbe15855fbe7cfe06b102633
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-07-15 17:52:37 -0300

    feat: :sparkles: añadir icono IconBox

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -360,3 +360,80 @@ export const IconUser = () => (
     <path d='M21 21v-2a4 4 0 0 0 -3 -3.85' />
   </svg>
 )
+
+export const IconCreditCard = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <rect width='20' height='14' x='2' y='5' rx='2' />
+    <line x1='2' x2='22' y1='10' y2='10' />
+  </svg>
+)
+
+export const IconBox = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='m7.5 4.27 9 5.15' />
+    <path d='M21 8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16Z' />
+    <path d='m3.3 7 8.7 5 8.7-5' />
+    <path d='M12 22V12' />
+  </svg>
+)
+
+export const IconFile = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z' />
+    <path d='M14 2v4a2 2 0 0 0 2 2h4' />
+    <path d='M10 9H8' />
+    <path d='M16 13H8' />
+    <path d='M16 17H8' />
+  </svg>
+)
+
+export const IconDolar = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <line x1='12' x2='12' y1='2' y2='22' />
+    <path d='M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6' />
+  </svg>
+)
