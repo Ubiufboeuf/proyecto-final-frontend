acc078919f13f17370366a844b0d332aa1cbeadb
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-07-17 16:52:33 -0300

    chore: :wrench: cambiar a output server y añadir adaptador de cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
+import cloudflare from '@astrojs/cloudflare'
 // import node from '@astrojs/node'
 
 // https://astro.build/config
@@ -14,5 +15,9 @@ export default defineConfig({
   },
 
   site: 'https://proyecto-final-frontend.pages.dev',
-  base: '/'
+  base: '/',
+  output: 'server',
+  adapter: cloudflare({
+    imageService: 'compile'
+  })
 })
