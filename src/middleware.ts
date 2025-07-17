9b169de91c34ed85eccd8c55db737b370d243126
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-07-16 23:00:24 -0300

    refactor: añadir keys de clerk en el middleware
    quiero ver si eso elimina el mensaje de que se están usando keys de desarrollo en producción

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,9 @@
 import { clerkMiddleware } from '@clerk/astro/server'
 
-export const onRequest = clerkMiddleware()
+const publishableKey = import.meta.env.PUBLIC_CLERK_PUBLISHABLE_KEY
+const secretKey = import.meta.env.CLERK_SECRET_KEY
+
+export const onRequest = clerkMiddleware({
+  publishableKey,
+  secretKey
+})
