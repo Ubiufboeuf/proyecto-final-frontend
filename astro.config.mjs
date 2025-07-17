a908194eeea6e0f677102c2d4988eb5d35c9b673
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-07-17 16:45:42 -0300

    chore: :wrench: cambiar output a static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
-import node from '@astrojs/node'
+// import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,9 +14,5 @@ export default defineConfig({
   },
 
   site: 'https://proyecto-final-frontend.pages.dev',
-  output: 'server',
-
-  base: '/',
-  
-  adapter: node({ mode: 'standalone' })
+  base: '/'
 })
