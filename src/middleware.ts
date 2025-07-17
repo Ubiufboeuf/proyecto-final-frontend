c3fdaad085bb77b5e68b39a6847c707668ab46fa
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-07-17 14:20:23 -0300

    revert: :fire: dejar como base el middleware de clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,3 @@
 import { clerkMiddleware } from '@clerk/astro/server'
 
-const publishableKey = import.meta.env.PUBLIC_CLERK_PUBLISHABLE_KEY
-const secretKey = import.meta.env.CLERK_SECRET_KEY
-
-export const onRequest = clerkMiddleware({
-  publishableKey,
-  secretKey
-})
+export const onRequest = clerkMiddleware()
