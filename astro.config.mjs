96462ab0b531a8a8c817fd6037d010a416c25486
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-07-16 22:09:19 -0300

    chore: cambios para arreglar el despliegue a producción

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,34 +3,21 @@ import { defineConfig } from 'astro/config'
 import preact from '@astrojs/preact'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
-import node from '@astrojs/node'
 import clerk from '@clerk/astro'
 // import { esUY } from '@clerk/localizations'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), sitemap(), clerk({
-    localization: {
-      signUp: {
-        start: {
-          title: 'lololol',
-          subtitle: 'xd?'
-        }
-      }
-    }
-  })],
+  integrations: [preact(), sitemap(), clerk()],
 
   vite: {
     plugins: [tailwindcss()]
   },
 
   site: 'https://proyecto-final-frontend.pages.dev',
-  output: 'server',
+  output: 'static',
 
-  base: '/',
+  base: '/'
   
   // adapter: node
-  adapter: node({
-    mode: 'standalone'
-  })
 })
