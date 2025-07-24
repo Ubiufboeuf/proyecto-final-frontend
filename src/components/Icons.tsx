551472c2eb45328ba7737111535aafecc9d59b5b
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-07-23 21:51:19 -0300

    feat: agregar icono de sofá y de tv

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -437,3 +437,41 @@ export const IconDolar = () => (
     <path d='M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6' />
   </svg>
 )
+
+export const IconSofa = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M19 9V6a2 2 0 0 0-2-2H7a2 2 0 0 0-2 2v3' />
+    <path d='M3 16a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5a2 2 0 0 0-4 0v2H7v-2a2 2 0 0 0-4 0Z' />
+    <path d='M5 18v2' />
+    <path d='M19 18v2' />
+  </svg>
+)
+
+export const IconTV = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    className='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <rect width='20' height='15' x='2' y='7' rx='2' ry='2' />
+    <polyline points='17 2 12 7 7 2' />
+  </svg>
+)
