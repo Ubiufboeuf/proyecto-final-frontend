8d840aa3263de59eedec317b698bc1c347129800
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-07-27 13:46:16 -0300

    feat: preparando para implementar leaflet para el seguimiento de ómnibus

--- src/components/tracking/MainZone.tsx
+++ src/components/tracking/MainZone.tsx
@@ -1,8 +1,9 @@
 import type { BusStates } from '@/env'
 import { useBusesStore } from '@/stores/useBusesStore'
-import { useEffect, useState } from 'preact/hooks'
+import { useEffect, useRef, useState } from 'preact/hooks'
 
 export function MainZone () {
+  const mapRef = useRef<HTMLDivElement>(null)
   const [state, setState] = useState<BusStates>('En viaje')
   const updateBusState = useBusesStore((state) => state.updateBusState)
   const setDelayed = useBusesStore((state) => state.setDelayed)
@@ -10,15 +11,15 @@ export function MainZone () {
   const setInTerminal = useBusesStore((state) => state.setInTerminal)
   const busesData = useBusesStore((state) => state.busesData)
   
-  function change () {
-    if (state === 'En viaje') {
-      setState('En terminal')
-    } else if (state === 'En terminal') {
-      setState('Atrasado')
-    } else {
-      setState('En viaje')
-    }
-  }
+  // function change () {
+  //   if (state === 'En viaje') {
+  //     setState('En terminal')
+  //   } else if (state === 'En terminal') {
+  //     setState('Atrasado')
+  //   } else {
+  //     setState('En viaje')
+  //   }
+  // }
 
   useEffect(() => {
     const bus = busesData[0]
@@ -39,7 +40,11 @@ export function MainZone () {
   return (
     <main class='h-full w-full absolute left-0 top-0 z-0 pt-16 pl-80'>
       <div class='h-full w-full flex justify-center items-center'>
-        <button onClick={change}>Cambiar primer bus entre estados</button>
+        <div
+          ref={mapRef}
+          id='map'
+          class='w-full h-full'
+        />
       </div>
     </main>
   )
