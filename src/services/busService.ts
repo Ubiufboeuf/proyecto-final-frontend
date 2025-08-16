e36d1ebcb51bc52a81ff6fcda8c426655f1660df
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-08-16 00:59:56 -0300

    feat: :sparkles: agregar función para obtener un objeto base de busesData

--- src/services/busService.ts
+++ src/services/busService.ts
@@ -1,112 +1,21 @@
 import type { BusesData, BusStates } from '@/env'
 
-const BUSES_DATA: BusesData = {
-  count: 12,
-  inMovement: 0,
-  selectedCount: 0,
-  inTerminal: 0,
-  delayed: 0,
-  timestamp: 0,
-  buses: [
-    {
-      id: 'b123',
-      state: 'En terminal',
-      driver: {
-        id: 'd123',
-        name: 'Bob Johnson'
-      },
-      destination: {
-        location: 'Punta del Este',
-        timestamp: 0
-      },
-      location: 'Canelones',
-      origin: {
-        location: 'Montevideo',
-        timestamp: 0
-      },
-      passengers: {
-        capacity: 48,
-        onBoard: 24
-      },
-      progress: 65,
-      selected: false
-    },
-    {
-      id: 'b234',
-      state: 'En terminal',
-      driver: {
-        id: 'd123',
-        name: 'Bob Johnson'
-      },
-      destination: {
-        location: 'Punta del Este',
-        timestamp: 0
-      },
-      location: 'Canelones',
-      origin: {
-        location: 'Montevideo',
-        timestamp: 0
-      },
-      passengers: {
-        capacity: 48,
-        onBoard: 24
-      },
-      progress: 65,
-      selected: false
-    },
-    {
-      id: 'b345',
-      state: 'En terminal',
-      driver: {
-        id: 'd123',
-        name: 'Bob Johnson'
-      },
-      destination: {
-        location: 'Punta del Este',
-        timestamp: 0
-      },
-      location: 'Canelones',
-      origin: {
-        location: 'Montevideo',
-        timestamp: 0
-      },
-      passengers: {
-        capacity: 48,
-        onBoard: 24
-      },
-      progress: 65,
-      selected: false
-    },
-    {
-      id: 'b456',
-      state: 'En terminal',
-      driver: {
-        id: 'd123',
-        name: 'Bob Johnson'
-      },
-      destination: {
-        location: 'Punta del Este',
-        timestamp: 0
-      },
-      location: 'Canelones',
-      origin: {
-        location: 'Montevideo',
-        timestamp: 0
-      },
-      passengers: {
-        capacity: 48,
-        onBoard: 24
-      },
-      progress: 65,
-      selected: false
-    }
-  ]
+export function getBaseBusesData (): BusesData {
+  return {
+    buses: [],
+    selectedCount: 0,
+    count: 0,
+    delayed: 0,
+    inMovement: 0,
+    inTerminal: 0,
+    timestamp: 0
+  }
 }
 
 export async function getBusesData () {
   const busStates: { [key in BusStates]: number } = { 'En terminal': 0, 'En viaje': 0, Atrasado: 0 }
 
-  const busesData: BusesData = { ...BUSES_DATA }
+  const busesData: BusesData = getBaseBusesData()
   const { buses } = busesData
   for (const busIdx in buses) {
     const bus = buses[busIdx]
