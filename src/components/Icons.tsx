5e5d0b8a53cf119cca7a474a8da26ce7175d51df
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-09-02 14:30:20 -0300

    feat: :sparkles: añadir propiedades para los iconos para el manejo de sus atributos

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -650,7 +650,7 @@ export const IconSend = () => (
   </svg>
 )
 
-export const IconMoon = () => (
+export const IconMoon = ({ hidden = false }: { hidden?: boolean }) => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
     width='24'
@@ -662,12 +662,13 @@ export const IconMoon = () => (
     strokeLinecap='round'
     strokeLinejoin='round'
     class='h-full w-full max-h-full max-w-full aspect-square'
+    hidden={hidden}
   >
     <path d='M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z' />
   </svg>
 )
 
-export const IconSun = () => (
+export const IconSun = ({ hidden = false }: { hidden?: boolean }) => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
     width='24'
@@ -679,13 +680,14 @@ export const IconSun = () => (
     strokeLinecap='round'
     strokeLinejoin='round'
     class='h-full w-full max-h-full max-w-full aspect-square'
+    hidden={hidden}
   >
     <path d='M12 12m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0' />
     <path d='M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7' />
   </svg>
 )
 
-export const IconComputer = () => (
+export const IconComputer = ({ hidden = false }: { hidden?: boolean }) => (
   <svg
     xmlns='http://www.w3.org/2000/svg'
     width='24'
@@ -697,6 +699,7 @@ export const IconComputer = () => (
     strokeLinecap='round'
     strokeLinejoin='round'
     class='h-full w-full max-h-full max-w-full aspect-square'
+    hidden={hidden}
   >
     <path d='M10 15h-6a1 1 0 0 1 -1 -1v-8a1 1 0 0 1 1 -1h6' />
     <path d='M13 4m0 1a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-6a1 1 0 0 1 -1 -1z' />
