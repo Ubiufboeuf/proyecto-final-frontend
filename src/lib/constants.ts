2ef6e539cf5544cff6373c521fc22a90cd4c4d71
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-09-02 17:27:11 -0300

    chore: leer variable de entorno para conseguir las rutas correctas

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,6 @@
+const origen = import.meta.env.ORIGIN
+
 export const ENDPOINTS = {
-  ROUTES: 'http://10.114.175.231:4321/mocks/routes.json',
-  BUSES_DATA: 'http://10.114.175.231:4321/mocks/buses_data.json'
+  ROUTES: `${origen}/mocks/routes.json`,
+  BUSES_DATA: `${origen}/mocks/buses_data.json`
 }
