634c95c8788cc6e65703eceeae71331416368d8f
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-09-14 22:56:49 -0300

    feat: :lipstick: terminar tema oscuro para la página de rutas

--- src/components/RoutesModal.tsx
+++ src/components/RoutesModal.tsx
@@ -29,13 +29,13 @@ export function RoutesModal () {
       onClick={handleHideModal}
       hidden={!isModalOpen}
     >
-      <main class='h-115 w-168 min-h-fit max-h-full bg-white rounded-lg text-neutral-700 overflow-y-auto p-5 px-7 gap-2 flex flex-col'>
-        <section class='h-fit flex items-center justify-between min-h-fit'>
+      <main class='h-115 w-168 min-h-fit max-h-full bg-white dark:bg-gray-800 rounded-lg text-neutral-700 dark:text-gray-300 overflow-y-auto p-5 px-7 gap-2 flex flex-col'>
+        <section class='h-fit flex flex-wrap-reverse items-center justify-between min-h-fit'>
           <div class='flex items-center gap-2'>
             <Icon class='size-6 text-orange-500'>
               <IconBus />
             </Icon>
-            <h1 class='text-lg font-bold text-gray-800 flex items-center'>
+            <h1 class='text-lg font-bold text-gray-800 dark:text-gray-50 flex items-center'>
               Horarios
               <div class='flex items-center gap-1'>
                 <span>: {origen}</span>
@@ -47,11 +47,11 @@ export function RoutesModal () {
             </h1>
           </div>
           <button
-            class='h-fit w-fit p-2 hover:bg-gray-200 touch:active:bg-gray-200 rounded-lg cursor-pointer'
+            class='h-fit w-fit p-2 hover:bg-gray-200 touch:active:bg-gray-200 dark:hover:bg-gray-700 dark:touch:active:bg-gray-700 rounded-lg cursor-pointer transition-colors ml-auto'
             title='Cerrar'
             onClick={hideModal}
           >
-            <Icon class='size-6 text-gray-800'>
+            <Icon class='size-6 text-gray-800 dark:text-gray-100'>
               <IconX />
             </Icon>
           </button>
@@ -59,8 +59,8 @@ export function RoutesModal () {
         <section class='w-full h-fit flex flex-col gap-4'>
           <div class='flex flex-col gap-2'>
             <div class='flex items-center gap-2'>
-              <div class='rounded-full bg-blue-500 size-3' />
-              <h1 class='text-blue-600 font-semibold'>
+              <div class='rounded-full bg-blue-500 dark:bg-blue-500/70 size-3' />
+              <h1 class='text-blue-600 dark:text-blue-500 font-semibold'>
                 Mañana (6:00 - 11:59)
               </h1>
             </div>
@@ -68,7 +68,7 @@ export function RoutesModal () {
               { horas?.map(({ hora, momento }) => momento === 'mañana' && (
                 <span
                   key={`modal-hora-${id}-${hora}`}
-                  class='h-fit w-fit px-3 p-1 rounded-full bg-blue-100 text-sm font-medium text-blue-700'
+                  class='h-fit w-fit px-3 p-1 rounded-full bg-blue-100 text-sm font-medium text-blue-700 dark:bg-gray-700/50 dark:text-blue-300'
                 >
                   {hora}
                 </span>
@@ -77,8 +77,8 @@ export function RoutesModal () {
           </div>
           <div class='flex flex-col gap-2'>
             <div class='flex items-center gap-2'>
-              <div class='rounded-full bg-green-500 size-3' />
-              <h1 class='text-green-700 font-semibold'>
+              <div class='rounded-full bg-green-500 dark:bg-green-500/70 size-3' />
+              <h1 class='text-green-700 dark:text-green-500/70 font-semibold'>
                 Tarde (12:00 - 17:59)
               </h1>
             </div>
@@ -86,7 +86,7 @@ export function RoutesModal () {
               { horas?.map(({ hora, momento }) => momento === 'tarde' && (
                 <span
                   key={`modal-hora-${id}-${hora}`}
-                  class='h-fit w-fit px-3 p-1 rounded-full bg-green-100 text-sm font-medium text-green-700'
+                  class='h-fit w-fit px-3 p-1 rounded-full bg-green-100 text-sm font-medium text-green-700 dark:bg-gray-700/50 dark:text-green-400/80'
                 >
                   {hora}
                 </span>
@@ -95,8 +95,8 @@ export function RoutesModal () {
           </div>
           <div class='flex flex-col gap-2'>
             <div class='flex items-center gap-2'>
-              <div class='rounded-full bg-purple-500 size-3' />
-              <h1 class='text-purple-600 font-semibold'>
+              <div class='rounded-full bg-purple-500 dark:bg-purple-500/70 size-3' />
+              <h1 class='text-purple-600 dark:text-purple-500/70 font-semibold'>
                 Noche (18:00 - 5:59)
               </h1>
             </div>
@@ -104,7 +104,7 @@ export function RoutesModal () {
               { horas?.map(({ hora, momento }) => momento === 'noche' && (
                 <span
                   key={`modal-hora-${id}-${hora}`}
-                  class='h-fit w-fit px-3 p-1 rounded-full bg-purple-100 text-sm font-medium text-purple-700'
+                  class='h-fit w-fit px-3 p-1 rounded-full bg-purple-100 text-sm font-medium text-purple-700 dark:bg-gray-700/50 dark:text-purple-400/80'
                 >
                   {hora}
                 </span>
@@ -113,21 +113,21 @@ export function RoutesModal () {
           </div>
         </section>
         <section class='w-full h-fit p-4 py-6 flex flex-col gap-1'>
-          <div class='text-sm text-gray-600 flex items-center w-full h-fit justify-between'>
+          <div class='text-sm text-gray-600 dark:text-gray-300 flex items-center w-full h-fit justify-between'>
             Total de servicios diarios:
             <span class='text-sm font-semibold text-gray-800'>{horas?.length ?? 0}</span>
           </div>
-          <div class='text-sm text-gray-600 flex items-center w-full h-fit justify-between'>
+          <div class='text-sm text-gray-600 dark:text-gray-300 flex items-center w-full h-fit justify-between'>
             Duración del viaje:
             <span class='text-sm font-medium text-gray-800'>{parseTimeInMinutes(duracion_en_minutos, 'h-mm')}</span>
           </div>
-          <div class='text-sm text-gray-600 flex items-center w-full h-fit justify-between'>
+          <div class='text-sm text-gray-600 dark:text-gray-300 flex items-center w-full h-fit justify-between'>
             Precio:
-            <span class='text-sm font-semibold text-orange-600'>${precio}</span>
+            <span class='text-sm font-semibold text-orange-600 dark:text-orange-500'>${precio}</span>
           </div>
         </section>
         <section class='w-full flex items-end justify-center'>
-          <p class='text-center text-sm text-gray-600'>💡 Los horarios pueden variar según la temporada. Confirma al comprar tu pasaje.</p>
+          <p class='text-center text-sm text-gray-600 dark:text-gray-300'>Los horarios pueden variar según la temporada. Confirma al comprar tu pasaje.</p>
         </section>
       </main>
     </dialog>
