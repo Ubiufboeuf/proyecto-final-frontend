9c70856d555a6e051554478359efbf4d73627955
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-09-15 16:06:06 -0300

    fix: :bug: arreglar store del tema
    eliminar valor inicial para agarrarlo de las cookies

--- src/stores/useThemeStore.ts
+++ src/stores/useThemeStore.ts
@@ -2,11 +2,11 @@ import type { ColorTheme } from '@/env'
 import { create } from 'zustand'
 
 type ThemeStore = {
-  theme: ColorTheme
+  theme: ColorTheme | undefined
   setTheme: (newTheme: ColorTheme) => void
 }
 
 export const useThemeStore = create<ThemeStore>((set) => ({
-  theme: 'system',
+  theme: undefined,
   setTheme: (newTheme) => set({ theme: newTheme })
 }))
