3c594f3967f499a8fb0b302b90100701da34e53a
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-09-25 17:36:46 -0300

    feat: crear componente para formularios FormTextArea

---
+++ src/components/forms/Textarea.tsx
@@ -0,0 +1,15 @@
+import type { FormTextarea } from '@/env'
+
+export function FormTextarea ({ detail: { id, placeholder, title } }: { detail: FormTextarea }) {
+  return (
+    <label class='flex flex-col gap-1 dark:text-gray-300 group col-start-1 col-end-3'>
+      <h1 class='text-gray-800 dark:text-gray-100 font-semibold sm:text-sm'>{title}</h1>
+      <textarea
+        id={id}
+        name={id}
+        placeholder={placeholder}
+        class='w-full h-fit min-h-[6lh] max-h-[12lh] field-sizing-content p-2.5 sm:text-sm rounded border resize-none cursor-pointer transition-colors border-gray-200 group-hover:border-gray-300 group-hover:bg-gray-100 dark:border-0 dark:bg-gray-700/50 dark:outline-0 dark:group-hover:bg-gray-700 dark:touch:active:bg-gray-700'
+      />
+    </label>
+  )
+}
