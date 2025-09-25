729936a556deadfda1c8e6460fbf075afd961e7b
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-09-25 17:38:29 -0300

    feat: :lipstick: arreglar tamaño de componentes de formulario en pantallas pequeñas

--- src/components/forms/Select.tsx
+++ src/components/forms/Select.tsx
@@ -3,11 +3,11 @@ import type { FormSelect } from '@/env'
 export function FormSelect ({ detail: { id: tripDetailId, options, title, defaultOption } }: { detail: FormSelect }) {
   return (
     <label class='flex flex-col gap-1 dark:text-gray-300 group'>
-      <h1 class='text-gray-800 dark:text-gray-100 font-semibold text-sm'>{title}</h1>
+      <h1 class='text-gray-800 dark:text-gray-100 font-semibold sm:text-sm'>{title}</h1>
       <select
         id={tripDetailId}
         name={tripDetailId}
-        class='w-full h-fit p-2.5 text-sm rounded border cursor-pointer transition-colors border-gray-200 group-hover:border-gray-300 dark:border-0 dark:bg-gray-700/50 dark:outline-0 dark:group-hover:bg-gray-700 dark:touch:active:bg-gray-700'
+        class='w-full h-fit p-2.5 sm:text-sm rounded border cursor-pointer transition-colors border-gray-200 group-hover:border-gray-300 dark:border-0 dark:bg-gray-700/50 dark:outline-0 dark:group-hover:bg-gray-700 dark:touch:active:bg-gray-700'
       >
         <option selected value='default' disabled>{defaultOption}</option>
         { options.map(({ id, name }) => (
--- src/components/forms/TimePicker.tsx
+++ src/components/forms/TimePicker.tsx
@@ -3,12 +3,12 @@ import type { FormTimePicker } from '@/env'
 export function FormTimePicker ({ detail: { title, id } }: { detail: FormTimePicker }) {
   return (
     <label class='flex flex-col gap-1 dark:text-gray-300 group'>
-      <h1 class='text-gray-800 dark:text-gray-100 font-semibold text-sm'>{title}</h1>
+      <h1 class='text-gray-800 dark:text-gray-100 font-semibold sm:text-sm'>{title}</h1>
       <input
         id={id}
         name={id}
         type='time'
-        class='w-full h-fit p-2.5 text-sm rounded border cursor-pointer transition-colors border-gray-200 group-hover:border-gray-300 dark:border-0 dark:bg-gray-700/50 dark:outline-0 dark:group-hover:bg-gray-700 dark:touch:active:bg-gray-700'
+        class='w-full h-fit p-2.5 sm:text-sm rounded border cursor-pointer transition-colors border-gray-200 group-hover:border-gray-300 dark:border-0 dark:bg-gray-700/50 dark:outline-0 dark:group-hover:bg-gray-700 dark:touch:active:bg-gray-700'
       />
     </label>
   )
