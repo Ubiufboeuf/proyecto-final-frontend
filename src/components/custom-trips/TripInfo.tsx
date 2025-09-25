5216735cb09e687ee4f39280fdddc804a259b1ae
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-09-25 17:37:06 -0300

    feat: actualizar componente TripInfo

--- src/components/custom-trips/TripInfo.tsx
+++ src/components/custom-trips/TripInfo.tsx
@@ -1,9 +1,16 @@
 import { tripInfo } from '@/lib/custom-trips'
+import { FormTextarea } from '../forms/Textarea'
 
 export function TripInfo () {
   return (
     <main class='grid md:grid-cols-2 grid-cols-1 gap-4'>
-
+      {
+        tripInfo.map((detail) => {
+          if (detail.type === 'textarea') {
+            return <FormTextarea key={`trip-detail-select-${detail.id}`} detail={detail} />
+          }
+        })
+      }
     </main>
   )
 }
