576c69ae294da01e967505cc3c1a0b52ccdd14fa
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-09-25 21:47:46 -0300

    feat: :lipstick: añadir color de fondo en hover y active en los TripTypes

--- src/components/custom-trips/TripTypes.tsx
+++ src/components/custom-trips/TripTypes.tsx
@@ -35,7 +35,7 @@ export function TripTypes () {
         tripTypes.map(({ id, title, description }) => (
           <label
             key={`trip-type-${id}`}
-            class='tripTypeElement flex flex-col h-fit w-full border-2 rounded-lg p-4 border-gray-200 dark:border-0 text-gray-600 dark:text-gray-300 dark:bg-gray-700/50 cursor-pointer transition-all hover:border-orange-500/50 has-checked:border-orange-400 has-checked:bg-orange-100 dark:has-checked:bg-gray-600'
+            class='tripTypeElement flex flex-col h-fit w-full border-2 rounded-lg p-4 border-gray-200 dark:border-0 text-gray-600 dark:text-gray-300 dark:bg-gray-700/50 cursor-pointer transition-all hover:border-orange-500/50 has-checked:border-orange-400 has-checked:bg-orange-100 dark:has-checked:bg-gray-600 dark:hover:bg-gray-600/50 dark:touch:active:bg-gray-600/50'
             onChange={handleChange(id)}
             data-id={id}
           >
