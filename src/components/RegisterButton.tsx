ca5d3ec86100c9e2d008f986236fb99e76b088d0
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-05 00:51:06 -0300

    feat: :sparkles: comenzar base para login y registro

--- src/components/RegisterButton.tsx
+++ src/components/RegisterButton.tsx
@@ -1,6 +1,6 @@
 export function RegisterButton ({ class: className = '' }: { class?: string }) {
   return (
-    <a class={`text-nowrap rounded-lg p-2 px-4 hover:bg-orange-600 touch:active:bg-orange-600 bg-orange-500 dark:bg-orange-600 dark:hover:bg-orange-600/80 dark:touch:active:bg-orange-600/80 text-white transition-colors cursor-pointer ${className}`}>
+    <a href='/register/' class={`text-nowrap rounded-lg p-2 px-4 hover:bg-orange-600 touch:active:bg-orange-600 bg-orange-500 dark:bg-orange-600 dark:hover:bg-orange-600/80 dark:touch:active:bg-orange-600/80 text-white transition-colors cursor-pointer ${className}`}>
       Registrarse
     </a>
   )
