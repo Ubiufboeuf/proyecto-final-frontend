90e180cc8c856df2b323385075f07efe90610f35
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-06 15:51:10 -0300

    feat: :sparkles: añadir propiedad hidden opcional al componente Icon

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,10 +1,11 @@
 import type { CSSProperties, ReactNode } from 'preact/compat'
 
-export function Icon ({ children, class: className, style }: { children: ReactNode, class?: string, style?: CSSProperties }) {
+export function Icon ({ children, class: className, style, hidden = false }: { children: ReactNode, class?: string, style?: CSSProperties, hidden?: boolean }) {
   return (
     <div 
       className={`icon max-h-full max-w-full aspect-square [&>*]:h-full [&>*]:w-full [&>*]:aspect-square [&>*]:max-h-full [&>*]:max-w-full [&>*]:select-none ${className}`}
       style={style}
+      hidden={hidden}
     >
       {children}
     </div>
