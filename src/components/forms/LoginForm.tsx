cd0fa332e12d303c5de56e026c39194e425dcac3
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-08 21:42:46 -0300

    feat: :sparkles: terminar formulario de login visualmente
    en realidad falta responsive, pero lo iba a juntar con los cambios de login/

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,8 +1,15 @@
 import { FormInput } from './Input'
 
 export function LoginForm () {
+  function handleSubmitForm (event: SubmitEvent) {
+    event.preventDefault()
+  }
+  
   return (
-    <form class='h-fit w-100 rounded-xl flex flex-col items-center gap-4 p-4 py-12 bg-gray-700/50'>
+    <form
+      class='h-fit w-100 rounded-xl flex flex-col items-center gap-4 p-4 py-12 bg-gray-700/50'
+      onSubmit={handleSubmitForm}
+    >
       <h1 class='text-gray-100 font-semibold text-xl'>Iniciar Sesión</h1>
       <div class='w-full px-6'>
         <FormInput
@@ -10,8 +17,9 @@ export function LoginForm () {
           name='input-register-password'
           title='Correo'
           placeholder='correo@email.com'
-          type='text'
+          type='email'
           class='text-base'
+          required
         />
       </div>
       <div class='w-full px-6'>
@@ -20,11 +28,16 @@ export function LoginForm () {
           name='input-login-password'
           title='Contraseña'
           placeholder='••••••••'
+          visiblePasswordPlaceholder='123456'
           type='password'
           class='text-base'
+          required
         />
       </div>
-      <div class='flex flex-col items-center mt-8'>
+      <button class='text-nowrap rounded-lg mt-4 p-2 px-4 hover:bg-orange-600 touch:active:bg-orange-600 bg-orange-500 dark:bg-orange-600 dark:hover:bg-orange-600/80 dark:touch:active:bg-orange-600/80 text-white transition-colors cursor-pointer'>
+        Iniciar Sesión
+      </button>
+      <div class='flex flex-col items-center mt-6'>
         <p class='text-gray-300'>¿No tienes una cuenta todavía?</p>
         <a
           href='/register/'
@@ -33,9 +46,6 @@ export function LoginForm () {
           Registrate
         </a>
       </div>
-      <button class='text-nowrap rounded-lg p-2 px-4 hover:bg-orange-600 touch:active:bg-orange-600 bg-orange-500 dark:bg-orange-600 dark:hover:bg-orange-600/80 dark:touch:active:bg-orange-600/80 text-white transition-colors cursor-pointer'>
-        Iniciar Sesión
-      </button>
     </form>
   )
 }
