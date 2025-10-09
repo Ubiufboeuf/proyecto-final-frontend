92845e9129c817b041bc4e0d0f9175537d41c56a
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-08 21:38:46 -0300

    feat: :sparkles: mejorar componente Input
    está quedando bastante bueno

--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -1,25 +1,50 @@
 import type { FormInput } from '@/env'
 import { Icon } from '../Icon'
 import { IconEye, IconEyeClosed } from '../Icons'
-import { useState, type HTMLInputTypeAttribute } from 'preact/compat'
+import { useRef, useState, type HTMLInputTypeAttribute } from 'preact/compat'
 
-export function FormInput ({ id, name, title, placeholder, type, class: className }: { id: string, name: string, title: string, type: HTMLInputTypeAttribute, placeholder: string, class: string }) {
+export function FormInput ({ id, name, title, placeholder, visiblePasswordPlaceholder = 'abc123', type, required = false, class: className }: { id: string, name: string, title: string, type: HTMLInputTypeAttribute, placeholder: string, visiblePasswordPlaceholder?: string, required?: boolean, class: string }) {
   const [showPassword, setShowPassword] = useState(false)
+  const inputRef = useRef<HTMLInputElement>(null)
 
   function toggleShowPassword () {
-    setShowPassword((prevState) => !prevState)
+    const input = inputRef.current
+    if (input) {
+      // Establecer foco en el input
+      setTimeout(() => input.focus(), 0)
+    }
+
+    setShowPassword((prevState) => { // prevState = showPassword, true o false
+      const mostrarContraseña = !prevState
+      // Debo usar !prevState porque es el que termina usando la UI como nuevo valor, así no queda desactualizado
+
+      if (input && type === 'password') input.placeholder = mostrarContraseña ? visiblePasswordPlaceholder : placeholder
+
+      return !prevState
+    })
+  }
+
+  function handleFocusInput () {
+    const input = inputRef.current
+    if (input) {
+      input.selectionStart = input.value.length
+      input.selectionEnd = input.value.length
+    }
   }
   
   return (
     <label class='relative flex flex-col gap-1 dark:text-gray-300 group'>
       <h1 class='text-gray-800 dark:text-gray-100 font-semibold sm:text-sm'>{title}</h1>
-      <div class={`${className} ${type === 'password' ? '' : 'input-password'} flex items-center justify-between gap-2 p-2.5 not-[.input-password]:pr-1 not-[.input-password]:py-1 w-full h-fit not-[[class*=title-]]:sm:text-sm rounded border-2 transition-colors border-gray-200 group-hover:border-orange-500/50 dark:border-transparent dark:bg-gray-700/50 dark:outline-0 dark:group-hover:border-gray-600 dark:touch:active:bg-gray-700 dark:has-[input:focus-visible]:bg-gray-800`}>
+      <div class={`${className} ${type === 'password' ? '' : 'input-password'} flex items-center justify-between gap-2 p-2.5 not-[.input-password]:pr-1 not-[.input-password]:py-1 w-full h-fit not-[[class*=title-]]:sm:text-sm rounded border-2 transition-colors border-gray-200 group-hover:border-orange-500/50 dark:border-transparent dark:bg-gray-700/50 dark:outline-0 dark:group-hover:border-gray-600 dark:touch:active:bg-gray-700 dark:focus-within:bg-gray-800`}>
         <input
+          ref={inputRef}
           id={id}
           name={name}
           type={type !== 'password' ? type : showPassword ? 'text' : type}
           placeholder={placeholder}
+          onFocus={handleFocusInput}
           class='h-full w-full outline-0 text-ellipsis'
+          required={required}
         />
         { type === 'password' && (
           <label
