e1b2b564f5c29c08c175ce4bffaaf58bb34e8e91
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-09 16:57:36 -0300

    feat: :lipstick: mejorar estilos del componente Textarea

--- src/components/forms/Textarea.tsx
+++ src/components/forms/Textarea.tsx
@@ -8,7 +8,7 @@ export function FormTextarea ({ detail: { id, placeholder, title } }: { detail: 
         id={id}
         name={id}
         placeholder={placeholder}
-        class='w-full h-fit min-h-[6lh] max-h-[12lh] field-sizing-content p-3 px-3.5 sm:text-sm rounded border-2 resize-none cursor-pointer transition-colors border-gray-200 group-hover:border-orange-500/50 dark:border-0 dark:bg-gray-700/50 dark:outline-0 dark:group-hover:bg-gray-700 dark:touch:active:bg-gray-700 dark:focus-visible:bg-gray-800 dark:focus-visible:hover:bg-gray-700'
+        class='w-full h-fit min-h-[6lh] max-h-[12lh] field-sizing-content p-3 px-3.5 sm:text-sm resize-none flex items-center justify-between gap-2 cursor-text rounded border-2 transition-colors border-gray-200 group-hover:border-orange-500/50 dark:bg-gray-700/50 outline-0 touch:group-active:border-orange-500/50 focus-within:bg-gray-100 dark:border-transparent dark:group-hover:border-gray-600 dark:touch:active:border-gray-600 dark:focus-within:bg-gray-800'
       />
     </label>
   )
