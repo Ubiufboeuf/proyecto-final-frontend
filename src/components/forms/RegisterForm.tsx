1c45bcde9bc9ebac7471f62cb834e146a3eb9380
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-08 22:51:42 -0300

    feat: :sparkles: terminar página de registro visualmente

---
+++ src/components/forms/RegisterForm.tsx
@@ -0,0 +1,62 @@
+import { FormInput } from './Input'
+
+export function RegisterForm () {
+  function handleSubmitForm (event: SubmitEvent) {
+    event.preventDefault()
+  }
+  
+  return (
+    <form
+      class='h-fit w-100 rounded-xl flex flex-col items-center gap-4 p-4 py-12 border border-gray-300 bg-white dark:border-0 dark:bg-gray-700/50'
+      onSubmit={handleSubmitForm}
+    >
+      <h1 class='text-gray-800 dark:text-gray-100 font-semibold text-xl'>Crea una cuenta</h1>
+      <div class='w-full lfw:px-6'>
+        <FormInput
+          id='input-register-name'
+          name='input-register-name'
+          title='Nombre'
+          placeholder='Pepito Martinez'
+          type='text'
+          class='text-base'
+          required
+        />
+      </div>
+      <div class='w-full lfw:px-6'>
+        <FormInput
+          id='input-register-email'
+          name='input-register-email'
+          title='Correo'
+          placeholder='correo@email.com'
+          type='email'
+          class='text-base'
+          required
+        />
+      </div>
+      <div class='w-full lfw:px-6'>
+        <FormInput
+          id='input-register-password'
+          name='input-register-password'
+          title='Contraseña'
+          placeholder='••••••••'
+          visiblePasswordPlaceholder='123456'
+          type='password'
+          class='text-base'
+          required
+        />
+      </div>
+      <button class='text-nowrap rounded-lg mt-4 p-2 px-4 hover:bg-orange-600 touch:active:bg-orange-600 bg-orange-500 dark:bg-orange-600 dark:hover:bg-orange-600/80 dark:touch:active:bg-orange-600/80 text-white transition-colors cursor-pointer'>
+        Crear cuenta
+      </button>
+      <div class='flex flex-col items-center mt-6'>
+        <p class='text-gray-600 dark:text-gray-300 text-center'>¿Ya tienes una cuenta?</p>
+        <a
+          href='/login/'
+          class='transition-colors underline decoration-2 underline-offset-2 text-blue-500 dark:text-blue-400 decoration-transparent hover:decoration-[unset] touch:active:decoration-[unset]'
+        >
+          Inicia sesión
+        </a>
+      </div>
+    </form>
+  )
+}
