b5a756430e0bcf33b35592090baf052486309cd1
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-13 13:31:06 -0300

    fix: :lipstick: arreglar color hover tema oscuro del textarea

--- src/components/forms/Textarea.tsx
+++ src/components/forms/Textarea.tsx
@@ -8,7 +8,7 @@ export function FormTextarea ({ id, name, placeholder, title, class: className }
         id={id}
         name={name}
         placeholder={placeholder}
-        class={`${className} w-full h-fit min-h-[6lh] max-h-[12lh] field-sizing-content p-3 px-3.5 not-[[class*=text]]:sm:text-sm resize-none flex items-center justify-between gap-2 cursor-text rounded border-2 transition-colors border-gray-200 group-hover:bg-orange-50 group-hover:border-orange-500/50 dark:bg-gray-700/50 outline-0 touch:group-active:border-orange-500/50 focus-within:bg-gray-100 dark:border-transparent dark:group-hover:border-gray-600 dark:touch:active:border-gray-600 dark:focus-within:bg-gray-800`}
+        class={`${className} w-full h-fit min-h-[6lh] max-h-[12lh] field-sizing-content p-3 px-3.5 not-[[class*=text]]:sm:text-sm resize-none flex items-center justify-between gap-2 cursor-text rounded border-2 transition-colors border-gray-200 group-hover:bg-orange-50 dark:group-hover:bg-transaprent group-hover:border-orange-500/50 dark:bg-gray-700/50 outline-0 touch:group-active:border-orange-500/50 focus-within:bg-gray-100 dark:border-transparent dark:group-hover:border-gray-600 dark:touch:active:border-gray-600 dark:focus-within:bg-gray-800`}
       />
     </label>
   )
