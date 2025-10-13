265fd62fe4a55267477cc978a1b3e6d82bce0276
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-12 21:07:57 -0300

    feat: :sparkles: implementar base del manejo de la store del usuario en UserLinks
    voy a ir primero con header y ya luego iré con sidebar, para ir más rápido

--- src/components/user/UserLinks.tsx
+++ src/components/user/UserLinks.tsx
@@ -1,13 +1,18 @@
 import { LoginLink } from './LoginLink'
 import { RegisterLink } from './RegisterLink'
 import { AccountLink } from './AccountLink'
+import { useUserStore } from '@/stores/useUserStore'
 
-export function UserLinks ({ display = 'header' }: { display: 'header' | 'sidebar' }) {  
+export function UserLinks ({ display = 'header' }: { display: 'header' | 'sidebar' }) {
+  const isAuth = useUserStore((state) => state.isAuth)
+  
   if (display === 'header') return (
     <div class='h-full w-fit not-xl:flex-1 flex items-center justify-end gap-2'>
-      <LoginLink class='not-xs:hidden xl:flex lg:hidden' />
-      <RegisterLink class='not-xs:hidden xl:flex lg:hidden' />
-      <AccountLink />
+      { !isAuth && <>
+        <LoginLink class='not-xs:hidden xl:flex lg:hidden' />
+        <RegisterLink class='not-xs:hidden xl:flex lg:hidden' />
+      </> }
+      { isAuth && <AccountLink /> }
     </div>
   )
 
