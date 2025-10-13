13e2741ea976200db444edf77e29571816973586
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-12 21:25:13 -0300

    feat: :sparkles: mejorar store del usuario

--- src/stores/useUserStore.ts
+++ src/stores/useUserStore.ts
@@ -1,11 +1,29 @@
 import { create } from 'zustand'
+import type { AuthStatus, User } from '@/env'
 
 type userStore = {
+  isInitialized: boolean
   isAuth: boolean
+  isLoading: boolean
+  user: User | null
+  initializeUser: (newAuthStatus: AuthStatus) => void
   setIsAuth: (newValue: boolean) => void
+  setIsLoading: (newValue: boolean) => void
+  setUser: (newUser: User) => void
 }
 
 export const useUserStore = create<userStore>((set) => ({
+  isInitialized: false,
   isAuth: false,
-  setIsAuth: (newValue) => set({ isAuth: newValue })
+  isLoading: false,
+  user: null,
+  initializeUser: ({ isAuth, isLoading, user }) => set({
+    isInitialized: true,
+    isAuth,
+    isLoading,
+    user
+  }),
+  setIsAuth: (newValue) => set({ isAuth: newValue }),
+  setIsLoading: (newValue) => set({ isLoading: newValue }),
+  setUser: (newUser) => set({ user: newUser })
 }))
