96cf2f15ff6526ec105c1525d3ef0f32ba8bf2bf
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-13 16:15:29 -0300

    fix: 🐛 arreglar problema de hidratación con temas del usuario

--- src/components/user/UserLinks.tsx
+++ src/components/user/UserLinks.tsx
@@ -7,6 +7,13 @@ import { SignedIn } from './SignedIn'
 
 export function UserLinks ({ display = 'header' }: { display: 'header' | 'sidebar' }) {
   const user = useUserStore((state) => state.user)
+  const isInitialized = useUserStore((state) => state.isInitialized)
+
+  if (!isInitialized) return (
+    <a class={`${display} [.header]:w-28 [.sidebar]:w-full h-10.5 bg-gray-200 dark:bg-gray-700 overflow-hidden rounded-lg loading`}>
+      <div />
+    </a>
+  )
 
   return (
     <>
@@ -28,7 +35,7 @@ export function UserLinks ({ display = 'header' }: { display: 'header' | 'sideba
               <LoginLink class='w-full text-center' />
             </SignedOut>
             <SignedIn>
-              <AccountLink username={user?.username} />
+              <AccountLink username={user?.username} class='w-full' />
             </SignedIn>
           </>
         )
