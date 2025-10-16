bbbca85f906f6164a4ad78b275b4c07cdfafd6c7
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-15 22:20:02 -0300

    refactor: :fire: eliminar prop pasado a AccountLink

--- src/components/user/UserLinks.tsx
+++ src/components/user/UserLinks.tsx
@@ -1,13 +1,10 @@
 import { LoginLink } from './LoginLink'
 import { RegisterLink } from './RegisterLink'
 import { AccountLink } from './AccountLink'
-import { useUserStore } from '@/stores/useUserStore'
 import { SignedOut } from './SignedOut'
 import { SignedIn } from './SignedIn'
 
 export function UserLinks ({ display = 'header' }: { display: 'header' | 'sidebar' }) {
-  const user = useUserStore((state) => state.user)
-
   return (
     <>
       {
@@ -18,7 +15,7 @@ export function UserLinks ({ display = 'header' }: { display: 'header' | 'sideba
               <RegisterLink class='not-xs:hidden xl:flex lg:hidden' />
             </SignedOut>
             <SignedIn>
-              <AccountLink username={user?.username} />
+              <AccountLink />
             </SignedIn>
           </div>
         ) : (
@@ -28,7 +25,7 @@ export function UserLinks ({ display = 'header' }: { display: 'header' | 'sideba
               <LoginLink class='w-full text-center' />
             </SignedOut>
             <SignedIn>
-              <AccountLink username={user?.username} class='w-full' />
+              <AccountLink class='w-full' />
             </SignedIn>
           </>
         )
