7bc448e04b105bb1a33bd62d3e1a8775e6391e60
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-02 12:30:06 -0300

    feat: :lipstick: igualar tamaño de botones principales con secundarios
    los naranja al lado de los transparentes con borde, se les añadió borde para tener el mismo tamaño

--- src/components/user/RegisterLink.tsx
+++ src/components/user/RegisterLink.tsx
@@ -1,6 +1,6 @@
 export function RegisterLink ({ class: className = '' }: { class?: string }) {
   return (
-    <a href='/register/' class={`text-nowrap rounded-lg p-2 px-4 hover:bg-orange-600 touch:active:bg-orange-600 bg-orange-500 dark:bg-orange-600 dark:hover:bg-orange-600/80 dark:touch:active:bg-orange-600/80 text-white transition-colors cursor-pointer ${className}`}>
+    <a href='/register/' class={`text-nowrap rounded-lg p-2 px-4 border hover:bg-orange-600 touch:active:bg-orange-600 bg-orange-500 border-orange-500 hover:border-orange-600 touch:active:border-orange-600 dark:bg-orange-600 dark:border-orange-600 dark:hover:bg-orange-600/80 dark:touch:active:bg-orange-600/80 dark:hover:border-[#CA430B] dark:touch:active:border-[#CA430B] text-white transition-colors cursor-pointer ${className}`}>
       Registrarse
     </a>
   )
