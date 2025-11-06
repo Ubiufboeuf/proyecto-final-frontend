732101b4576befa2bb4cd706509b74a53045b832
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-06 14:05:24 -0300

    feat: agregar icono de candado abierto

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -809,3 +809,21 @@ export const IconLock = () => (
     <path d='M8 11v-4a4 4 0 1 1 8 0v4' />
   </svg>
 )
+
+export const IconLockOpen = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='24'
+    height='24'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='currentColor'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    class='h-full w-full max-h-full max-w-full aspect-square'
+  >
+    <path d='M3 13a2 2 0 0 1 2 -2h10a2 2 0 0 1 2 2v6a2 2 0 0 1 -2 2h-10a2 2 0 0 1 -2 -2z' />
+    <path d='M13 11v-4a4 4 0 1 1 8 0v4' />
+  </svg>
+)
