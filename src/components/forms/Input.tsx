32b8a22488ea40c4f76c2a99a516d15fd0763975
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-07 13:43:04 -0300

    fix: arreglar clase del componente Input
    creo que es arreglar, no sé ni dónde se aprovechaba

--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -40,7 +40,7 @@ export function FormInput ({
   return (
     <label class={`${wrapperClass} relative flex flex-col gap-1 text-gray-700 dark:text-gray-300 group`}>
       <h1 class='text-gray-800 dark:text-gray-100 font-semibold sm:text-sm'>{title}</h1>
-      <div class={`${className} ${type === 'password' ? '' : 'input-password'} flex items-center justify-between gap-2 w-full h-fit not-[[class*=title-]]:sm:text-sm rounded border-2 transition-colors border-gray-200 hover:bg-orange-50 focus-within:hover:bg-gray-100 dark:hover:bg-gray-700/50 dark:focus-within:hover:bg-gray-800 hover:border-orange-500/50 touch:group-active:border-orange-500/50 focus-within:bg-gray-100 dark:border-transparent dark:bg-gray-700/50 dark:outline-0 dark:hover:border-gray-600 dark:touch:active:border-gray-600 dark:focus-within:bg-gray-800 cursor-text`}>
+      <div class={`${className} ${type === 'password' ? '' : 'input-password'} flex items-center justify-between gap-2 w-full h-fit not-[[class*=text-]]:sm:text-sm rounded border-2 transition-colors border-gray-200 hover:bg-orange-50 focus-within:hover:bg-gray-100 dark:hover:bg-gray-700/50 dark:focus-within:hover:bg-gray-800 hover:border-orange-500/50 touch:group-active:border-orange-500/50 focus-within:bg-gray-100 dark:border-transparent dark:bg-gray-700/50 dark:outline-0 dark:hover:border-gray-600 dark:touch:active:border-gray-600 dark:focus-within:bg-gray-800 cursor-text`}>
         <input
           ref={inputRef}
           id={id}
