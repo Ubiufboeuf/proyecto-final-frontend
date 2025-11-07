afd2e77f680de4d5c794ecb975055d2998938530
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-07 13:40:39 -0300

    fix: :lipstick: arreglar estilos del componente textarea

--- src/components/forms/Textarea.tsx
+++ src/components/forms/Textarea.tsx
@@ -8,7 +8,7 @@ export function FormTextarea ({ id, name, placeholder, title, class: className }
         id={id}
         name={name}
         placeholder={placeholder}
-        class={`${className} w-full h-fit min-h-[6lh] max-h-[12lh] field-sizing-content p-3 px-3.5 not-[[class*=text]]:sm:text-sm resize-none flex items-center justify-between gap-2 cursor-text rounded border-2 transition-colors border-gray-200 hover:bg-orange-50 dark:hover:bg-transparent hover:border-orange-500/50 dark:bg-gray-700/50 outline-0 touch:group-active:border-orange-500/50 focus-within:bg-gray-100 dark:border-transparent dark:hover:border-gray-600 dark:touch:active:border-gray-600 dark:focus-within:bg-gray-800`}
+        class={`${className} flex items-center justify-between gap-2 w-full h-fit min-h-[6lh] max-h-[12lh] field-sizing-content resize-none p-3 px-3.5 not-[[class*=text-]]:sm:text-sm rounded border-2 transition-colors border-gray-200 hover:bg-orange-50 focus-within:hover:bg-gray-100 dark:hover:bg-gray-700/50 dark:focus-within:hover:bg-gray-800 hover:border-orange-500/50 touch:group-active:border-orange-500/50 focus-within:bg-gray-100 dark:border-transparent dark:bg-gray-700/50 dark:outline-0 dark:hover:border-gray-600 dark:touch:active:border-gray-600 dark:focus-within:bg-gray-800 cursor-text`}
       />
     </label>
   )
