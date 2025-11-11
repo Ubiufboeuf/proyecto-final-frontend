653c53aa2db0c2a79c595179aa03d99f91f17973
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-11 12:50:58 -0300

    refactor: cambiar token de pruebas
    es para usar un usuario chofer

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -2,7 +2,7 @@ import { SESSION_COOKIE_NAME } from '@/lib/constants'
 import type { APIRoute } from 'astro'
 
 export const POST: APIRoute = async ({ cookies, redirect }) => {  
-  cookies.set(SESSION_COOKIE_NAME, 'token2', {
+  cookies.set(SESSION_COOKIE_NAME, 'token1', {
     path: '/',
     httpOnly: true,
     sameSite: 'strict',
