bacb6caf670ba4e5e87dfbd75e0350039f39a725
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-17 15:47:13 -0300

    refactor: evitar autenticar el usuario si no es una pagina para autenticarlo

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,16 +4,17 @@ import { validateToken } from './services/userService'
 import type { User } from './env'
 import { SESSION_COOKIE_NAME } from './lib/constants'
 
-const auth = defineMiddleware(async (context, next) => {
-  // Esto es para crear una cookie para pruebas, pero igual, algo así debería ser la configuración de la real
-  // context.cookies.set(SESSION_COOKIE_NAME, 'token2', {
-  //   httpOnly: true,
-  //   sameSite: 'strict',
-  //   path: '/',
-  //   secure: true
-  // })
-  const token = context.cookies.get(SESSION_COOKIE_NAME)
+const routesToAuth = [
+  '/me/',
+  '/me',
+  '/driver/',
+  '/driver'
+]
 
+const auth = defineMiddleware(async (context, next) => {
+  const { pathname } = context.url
+  const token = context.cookies.get(SESSION_COOKIE_NAME)
+  
   if (!token) {
     context.locals.isLoading = false
     context.locals.isAuth = false
@@ -22,6 +23,15 @@ const auth = defineMiddleware(async (context, next) => {
     return next()
   }
 
+  // Si la ruta no es para autenticar, entonces evita conseguir los datos del usuario
+  if (!routesToAuth.includes(pathname)) {
+    context.locals.isLoading = false
+    context.locals.isAuth = true
+    context.locals.user = null
+    
+    return next()
+  }
+  
   // isLoading es para diferenciar el false de isAuth de "sin verificar" y "verificado fallido"
   let user: User | null = null
   let isAuth: boolean = false
