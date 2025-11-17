a79af1d08e0f86777900ab8041608a734bfdf5dd
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-11-17 13:16:40 -0300

    fix: :bug: quitar configuración https
    da problemas con el servidor php

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,25 @@ import preact from '@astrojs/preact'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import cloudflare from '@astrojs/cloudflare'
-import { readFileSync } from 'node:fs'
+// import { readFileSync } from 'node:fs'
 // import node from '@astrojs/node'
 
-let httpsConfig = {}
-try {
-  const cert = readFileSync('.cert/cert.pem')
-  const key = readFileSync('.cert/key.pem')
-  httpsConfig = { server: { https: { cert, key } } }
-} catch {
-  console.warn('- No se encontraron certificados, se levanta HTTP -')
-}
+// let httpsConfig = {}
+// try {
+//   const cert = readFileSync('.cert/cert.pem')
+//   const key = readFileSync('.cert/key.pem')
+//   httpsConfig = { server: { https: { cert, key } } }
+// } catch {
+//   console.warn('- No se encontraron certificados, se levanta HTTP -')
+// }
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), sitemap()],
 
   vite: {
-    plugins: [tailwindcss()],
-    ...httpsConfig
+    plugins: [tailwindcss()]
+    // ...httpsConfig
   },
   server: {
     allowedHosts: true
